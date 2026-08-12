949f99da6eb0fef6f39bb934e329b2cc82751bbe
MrgaCRO <61421552+MrgaCRO@users.noreply.github.com>
2026-08-12 13:28:24 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const cacheName = 'snake-edu-v1.4.0'; // Nadograđeno na v1.4.0 - Praćenje miša i WASD kontrole
+const assets = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
